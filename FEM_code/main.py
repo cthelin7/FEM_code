89a89af8919b994d417190da96c1bf8978de7da9
Christopher Thelin <cthelin21@gmail.com>
2018-10-15 10:45:39 -0600

    created LM matrix from ID and IEN matrices

--- FEM_code/main.py
+++ FEM_code/main.py
@@ -10,7 +10,7 @@ h = -1.0
 # -ux(0) = 0
 
 num_elements = 4
-nodes_per_element = 3
+nodes_per_element = 2
 num_nodes = nodes_per_element*num_elements - (num_elements - 1)
 he = 1/num_elements
 node_ids = []
@@ -44,23 +44,29 @@ K = np.zeros((len(active_nodes), len(active_nodes)), dtype=np.float16)
 # create ID matrix
 ID = np.zeros((2, len(Nodes.nodes_list)), dtype=np.float16)
 glob_eq_id = 1
-for i, node_id in enumerate(node_ids):
-    ID[0][i] = node_id
+for n, node_id in enumerate(node_ids):
+    ID[0][n] = node_id
     if node_id is ng_id:
-        ID[1][i] = 0
+        ID[1][n] = 0
     else:
-        ID[1][i] = glob_eq_id
+        ID[1][n] = glob_eq_id
         glob_eq_id += 1
 
 # create IEN matrix (map global node ids to element ids and local node ids
 IEN = np.zeros((nodes_per_element, num_elements), dtype=int)
-for i in range(0, len(Elements.elements_list)):
-    for j in range(0, nodes_per_element):
-        IEN[j][i] = Elements.elements_list[i].nodes[j].node_id
+for e in range(0, len(Elements.elements_list)):
+    for a in range(0, nodes_per_element):
+        IEN[a][e] = Elements.elements_list[e].nodes[a].node_id
 
 # create LM matrix which maps to global equation numbers
+LM = np.zeros((nodes_per_element, num_elements), dtype=int)
+for e in range (0, len(Elements.elements_list)):
+    for a in range(0, nodes_per_element):
+        for i in range(0, len(ID[0])):
+            if IEN[a][e] == ID[0][i]:
+                LM[a][e] = ID[1][i]        # likely a better numpy way to do this
 
-for e in Elements.elements_list[0]:
+for e in range(0, num_elements):
     for a in range(0, nodes_per_element):
         for b in range(0, num_elements):
             # calc ke
